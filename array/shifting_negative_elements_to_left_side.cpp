a9ab83ce74571a294b1f39e026b519bea9d5e5ab
Cynical <26singhnikki@gmail.com>
2022-04-08 02:59:20 +0530

    April 08

---
+++ array/shifting_negative_elements_to_left_side.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ //using partition algorithm , we take a pivot and then shift the elements 
+int main()
+{
+    int n ;
+    cin >>  n ;
+    int pivot = 0; 
+    int a[n];
+    for (int i=0 ; i <n;i++){
+        cin>>  a[i];
+    }
+    int i =-1;
+    for (int j=0; j<n;j++){
+        if (a[j]<0){
+            i++;
+            swap(a[i],a[j]);
+        }
+    }
+    for(int i=0 ; i <n ; i++)
+        cout<<a[i]<<endl;
+    return 0;
+}
